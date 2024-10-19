159a8953feac882ba41c2068ee34aa2237c390e7
Mempa21 <mempa1peu@gmail.com>
2024-10-19 14:39:40 +0200

    change secrets

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtStrategy } from './jwt.strategy';
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
+      secret: '7dd7b9aad0c0bbbead0c6b0adb9a21fe',
       signOptions: { expiresIn: '60m' },
     }),
   ],
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: '7dd7b9aad0c0bbbead0c6b0adb9a21fe',
     });
   }
 
